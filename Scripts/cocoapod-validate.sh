dd75a13b572b23ed5bc6bd190a438193001f3690
Dov Frankel <dov@abbey-code.com>
2017-09-15 08:24:42 -0400

    Added 'pod env' call to cocoapod-validate script, to include in CocoaPods project issue reports (Issue #59)

--- Scripts/cocoapod-validate.sh
+++ Scripts/cocoapod-validate.sh
@@ -6,6 +6,12 @@ set -ev
 
 pod --version
 
+echo Getting CocoaPods environment...
+
+pod env
+
+echo Done getting CocoaPods environment.
+
 # Lint the podspec to check for errors. Don't call `pod spec lint`, because we want it to evaluate locally
 pod lib lint --verbose
 
