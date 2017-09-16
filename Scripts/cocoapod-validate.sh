d32362cadd1e6dde45031b55705da7131a796830
Dov Frankel <dov@abbey-code.com>
2017-09-16 11:17:58 -0400

    Fixed script syntax

--- Scripts/cocoapod-validate.sh
+++ Scripts/cocoapod-validate.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-. Scripts/set-travis-tag-to-latest.sh
+./Scripts/set-travis-tag-to-latest.sh
 
 pod --version
 
@@ -15,4 +15,4 @@ echo Done getting CocoaPods environment.
 # Lint the podspec to check for errors. Don't call `pod spec lint`, because we want it to evaluate locally
 pod lib lint --verbose
 
-. Scripts/unset-travis-tag.sh
+./Scripts/unset-travis-tag.sh
