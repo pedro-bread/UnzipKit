bb426a50e4380483ae2b465a634b48d314b93567
Dov Frankel <dov@abbey-code.com>
2017-09-16 20:28:14 -0400

    Removed unnecessary echoes

--- Scripts/cocoapod-validate.sh
+++ Scripts/cocoapod-validate.sh
@@ -6,11 +6,7 @@ set -ev
 
 pod --version
 
-echo Getting CocoaPods environment...
-
 pod env
-
-echo Done getting CocoaPods environment.
 
 # Lint the podspec to check for errors. Don't call `pod spec lint`, because we want it to evaluate locally
 pod lib lint --verbose
